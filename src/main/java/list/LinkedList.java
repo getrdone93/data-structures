4c311d78eef83af484acff93681c7c529370ea14
Trever Anderson <treveranderson01@gmail.com>
2020-02-05 20:29:02 -0600

    unit test passes. implemented linkedlist add, get, delete, size

--- src/main/java/list/LinkedList.java
+++ src/main/java/list/LinkedList.java
@@ -4,45 +4,70 @@ import common.Node;
 
 public class LinkedList<E> {
 
-	private Node<E> head;
-	private long length;
-	
-	public Node<E> addNode(E data) {
-		if (this.head == null) {
-			this.head = new Node<E>(data);
-		} else {
-			Node<E> currentNode = this.head;
-			while (currentNode.getNext() != null) {
-				currentNode = currentNode.getNext();
-			}
-			currentNode.setNext(new Node<E>(data));
-		}
-		this.length++;
-		return this.head;
-	}
-	
-	public E get(long index) {
-		Node<E> copyHead = this.head;
-		long i = 0;
-		while (copyHead.getNext() != null && i < index) {
-			copyHead = copyHead.getNext();
-			i++;
-		}
-		return i == index ? copyHead.getData() : null;
-	}
-		
-	public long getLength() {
-		return this.length;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder result = new StringBuilder();
-		Node<E> copyHead = this.head;
-		while (copyHead.getNext() != null) {
-			result.append(copyHead.getData());
-		}
-		
-		return result.toString();
-	}
+    private Node<E> head;
+    private int length;
+
+    public Node<E> add(E data) {
+        if (this.head == null) {
+            this.head = new Node<E>(data);
+        } else {
+            Node<E> currentNode = this.head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(new Node<E>(data));
+        }
+        this.length++;
+        return this.head;
+    }
+
+    public E get(long index) {
+        Node<E> copyHead = this.head;
+        long i = 0;
+        while (i < index && copyHead.getNext() != null) {
+            copyHead = copyHead.getNext();
+            i++;
+        }
+        return i == index ? copyHead.getData() : null;
+    }
+
+    public E remove(long index) {
+        if (index == 0) {
+            E copy = this.head.getData();
+            this.head = this.head.getNext();
+            this.length--;
+            return copy;
+        } else {
+            Node<E> copyHead = this.head;
+            Node<E> previous = copyHead;
+            long i = 0;
+            while (i < index && copyHead.getNext() != null) {
+                previous = copyHead;
+                copyHead = copyHead.getNext();
+                i++;
+            }
+            if (i == index && copyHead != null) {
+                previous.setNext(copyHead.getNext());
+                this.length--;
+                return copyHead.getData();
+            }
+        }
+
+        return null;
+    }
+
+    public int size() {
+        return this.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        Node<E> copyHead = this.head;
+        while (copyHead.getNext() != null) {
+            result.append(copyHead.getData());
+        }
+
+        return result.toString();
+    }
 }
--- src/test/java/tests/LinkedListTest.java
+++ src/test/java/tests/LinkedListTest.java
@@ -1,21 +1,63 @@
 package tests;
 
 import org.junit.Test;
-
-import list.LinkedList;
+import org.junit.Assert;
 
 public class LinkedListTest {
 
-	@Test
-	public void testLinkedList() {
-		final int nums = 10;
-		LinkedList<Integer> list = new LinkedList<>();
-		for (int i = 0; i < nums; i++) {
-			list.addNode(Integer.valueOf(i));
-		}
-		
-		for (int i = 0; i < nums; i++) {
-			System.out.printf("i: %d, value: %d\n", i, list.get(i));
-		}
-	}
+    private <E> void listsEqual(java.util.LinkedList<E> expected, list.LinkedList<E> actual) {
+        Assert.assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testLinkedListSize() {
+        final int nums = 5;
+        list.LinkedList<Integer> actual = new list.LinkedList<>();
+
+        for (int i = 0; i < nums; i++) {
+            actual.add(i);
+        }
+
+        Assert.assertEquals(5L, actual.size());
+    }
+
+    @Test
+    public void testLinkedListAdd() {
+        final int nums = 5;
+        list.LinkedList<Integer> actual = new list.LinkedList<>();
+        java.util.LinkedList<Integer> expected = new java.util.LinkedList<>();
+
+        for (int i = 0; i < nums; i++) {
+            actual.add(i);
+            expected.add(i);
+        }
+
+        listsEqual(expected, actual);
+    }
+
+    @Test
+    public void testLinkedListDelete() {
+        final int nums = 8;
+        list.LinkedList<Integer> actual = new list.LinkedList<>();
+        java.util.LinkedList<Integer> expected = new java.util.LinkedList<>();
+
+        for (int i = 0; i < nums; i++) {
+            actual.add(i);
+            expected.add(i);
+        }
+
+        actual.remove(0);
+        expected.remove(0);
+
+        actual.remove(actual.size() - 1);
+        expected.remove(expected.size() - 1);
+
+        actual.remove(actual.size() / 2);
+        expected.remove(expected.size() / 2);
+
+        listsEqual(expected, actual);
+    }
 }
